ced34318736fff0747d498a76f30e380b02bb00e
lamid-codes <tioluwalase38@gmail.com>
2025-11-28 20:40:15 +0100

    Set up Express app with routes and middleware

--- assessment-week-5/app.js
+++ assessment-week-5/app.js
@@ -1,1 +1,11 @@
-
+const express = require("express");
+const app = express();
+const bookRoutes = require("./routes/bookRoutes");
+const logger = require("./middleware/logger");
+app.use(express.json());
+app.use(logger);
+app.use("/api/books", bookRoutes);
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+module.exports = app;
